aa00c3014aec79807b821ba34a30e7760ed90116
Alex <alex.fursal_m@outlook.com>
2022-05-19 11:42:08 +0300

    newWindows

--- info/hotels/Coliseum/hotels.js
+++ info/hotels/Coliseum/hotels.js
@@ -8,23 +8,33 @@ function btnClick() {
 }
 
 function btnClick1() {
-  location.href =
-    '//www.booking.com/hotel/it/colosseum-palace-star.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55#availability_target';
+  // location.href =
+  //   '//www.booking.com/hotel/it/colosseum-palace-star.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55#availability_target';
+
+    myWin = open("//www.booking.com/hotel/it/colosseum-palace-star.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55#availability_target");
 }
 function btnClick2() {
-  location.href =
-    '//www.booking.com/hotel/it/dolce-casa-colosseo.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55';
+  // location.href =
+  //   '//www.booking.com/hotel/it/dolce-casa-colosseo.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55';
+
+    myWin = open("//www.booking.com/hotel/it/dolce-casa-colosseo.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55");
 }
 
 function btnClick3() {
-  location.href =
-    'https://www.booking.com/hotel/it/residenza-roma-imperiale.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55';
+  // location.href =
+  //   'https://www.booking.com/hotel/it/residenza-roma-imperiale.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55';
+
+    myWin = open("//www.booking.com/hotel/it/residenza-roma-imperiale.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55");
 }
 
 function btnClick4() {
-  location.href =
-    'https://www.booking.com/hotel/it/colosseum-music-design.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55';
-}
+  // location.href =
+  //   'https://www.booking.com/hotel/it/colosseum-music-design.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55';
+
+    myWin = open("//www.booking.com/hotel/it/colosseum-music-design.ru.html?aid=348397;label=colosseum-cYY1r0mil4dxiepNph%2Al4AS275066797610%3Apl%3Ata%3Ap18100%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666475%3Akwd-38985253688%3Alp1030310%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfOICo1LOVmeLYIuzED8KG8;sid=b386882b6121641273ae0b29bcc55b55");
+
+    // myWin = open("/info/Europe.html");
+  }
 
 btn1.addEventListener('click', btnClick1);
 btn2.addEventListener('click', btnClick2);
